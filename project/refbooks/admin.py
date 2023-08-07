de07fccff140f25f3bd5e41f83cdb34f66d43518
Ilya <ilia.barinov2012@gmail.com>
2023-08-08 01:59:36 +0500

    Изменил имена полей *_id, реализовал админ панель

--- project/refbooks/admin.py
+++ project/refbooks/admin.py
@@ -1,3 +1,53 @@
+from datetime import date
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Refbook, RefbookVersion, RefbookElement
+
+@admin.register(Refbook)
+class RefbookAdmin(admin.ModelAdmin):
+    """
+    Панель справочников
+    """
+    class VersionInline(admin.TabularInline):
+        model = RefbookVersion
+    inlines = (VersionInline,)
+
+    def current_version(self, obj):
+        current_version = RefbookVersion.objects.filter(refbook=obj, date__lte=date.today()).latest('date')
+        return current_version.version
+    current_version.short_description = 'Текущая версия'
+
+    def current_version_date(self, obj):
+        current_version = RefbookVersion.objects.filter(refbook=obj, date__lte=date.today()).latest('date')
+        return current_version.date
+    current_version_date.short_description = 'Дата начала действия версии'
+
+    list_display = ('id', 'code', 'name', 'current_version', 'current_version_date')
+
+
+@admin.register(RefbookVersion)
+class RefbookVersionAdmin(admin.ModelAdmin):
+    """
+    Панель версии справочника
+    """
+    class RefbookElementInline(admin.TabularInline):
+        model = RefbookElement
+    inlines = (RefbookElementInline,)
+
+    def refbook_code(self, obj):
+        refbook = Refbook.objects.get(id=obj.refbook.id)
+        return refbook.code
+    refbook_code.short_description = 'Код справочника'
+
+    def refbook_name(self, obj):
+        refbook = Refbook.objects.get(id=obj.refbook.id)
+        return refbook.name
+    refbook_name.short_description = 'Наименование справочника'
+
+    list_display = ('refbook_code', 'refbook_name', 'version', 'date')
+
+
+@admin.register(RefbookElement)
+class RefbookElementAdmin(admin.ModelAdmin):
+    pass
--- project/refbooks/models.py
+++ project/refbooks/models.py
@@ -2,27 +2,35 @@ from django.db import models
 
 
 class Refbook(models.Model):
-    id = models.AutoField(primary_key=True)
-    code = models.CharField(max_length=100, unique=True)
-    name = models.CharField(max_length=300)
-    description = models.TextField(blank=False)
+    id = models.AutoField(primary_key=True, verbose_name="Идентификатор")
+    code = models.CharField(max_length=100, unique=True, verbose_name="Код")
+    name = models.CharField(max_length=300, verbose_name="Наименование")
+    description = models.TextField(blank=True, verbose_name="Описание")
+
+    class Meta:
+        verbose_name = "Справочник"
+        verbose_name_plural = "Справочники"
 
 
 class RefbookVersion(models.Model):
-    id = models.AutoField(primary_key=True)
-    refbook_id = models.ForeignKey(Refbook, on_delete=models.CASCADE)
-    version = models.CharField(max_length=50)
-    date = models.DateField()
+    id = models.AutoField(primary_key=True, verbose_name="Идентификатор")
+    refbook = models.ForeignKey(Refbook, on_delete=models.CASCADE, verbose_name="Справочник")
+    version = models.CharField(max_length=50, verbose_name="Версия")
+    date = models.DateField(verbose_name="Дата начала действия")
 
     class Meta:
+        verbose_name = "Версия справочника"
+        verbose_name_plural = "Версии справочников"
         unique_together = (('refbook_id', 'version'), ('refbook_id', 'date'))
 
 
 class RefbookElement(models.Model):
-    id = models.AutoField(primary_key=True)
-    version_id = models.ForeignKey(RefbookVersion, on_delete=models.CASCADE)
-    code = models.CharField(max_length=100)
-    value = models.CharField(max_length=300)
+    id = models.AutoField(primary_key=True, verbose_name="Идентификатор")
+    version = models.ForeignKey(RefbookVersion, on_delete=models.CASCADE, verbose_name="Версия")
+    code = models.CharField(max_length=100, verbose_name="Код элемента")
+    value = models.CharField(max_length=300, verbose_name="Значение элемента")
 
     class Meta:
+        verbose_name = "Элемент справочника"
+        verbose_name_plural = "Элементы справочников"
         unique_together = (('version_id', 'code'),)
