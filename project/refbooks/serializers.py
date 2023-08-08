3fdad1ef6374e69dfe6b4fa1ac5ae9ed57d8df76
Ilya <ilia.barinov2012@gmail.com>
2023-08-08 20:43:25 +0500

    Убрал вывод описания API Refbook

--- project/refbooks/serializers.py
+++ project/refbooks/serializers.py
@@ -8,15 +8,7 @@ from .models import Refbook, RefbookElement
 class RefbookAPISerializer(serializers.ModelSerializer):
     class Meta:
         model = Refbook
-        fields = '__all__'
-
-    def to_representation(self, instance):
-        """
-        Метод для очистки пустых полей
-        """
-        result = super().to_representation(instance)
-        result = OrderedDict([(key, result[key]) for key in result if result[key]])
-        return result
+        fields = ('id', 'code', 'name')
 
 
 class RefbookElementAPISerializer(serializers.ModelSerializer):
