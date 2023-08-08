72ec06cd19e96e77cff8600ea5e6ded753f99c85
Ilya <ilia.barinov2012@gmail.com>
2023-08-08 19:14:28 +0500

    Изменил описание swagger

--- project/project/yasg.py
+++ project/project/yasg.py
@@ -6,11 +6,10 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Сервис терминологии",
       default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
+      description="Тестовое задание для стажировки (python)",
+      contact=openapi.Contact(name="Илья Баринов", email="ilia.barinov2012@gmail.com"),
       license=openapi.License(name="BSD License"),
    ),
    public=True,
