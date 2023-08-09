3114f7b6157c6534f9c804c18a13c67b9a7bfe8b
Ilya <ilia.barinov2012@gmail.com>
2023-08-09 14:45:49 +0500

    Протестировал API RefbookElements

--- project/refbooks/tests.py
+++ project/refbooks/tests.py
@@ -2,7 +2,7 @@ from rest_framework.reverse import reverse
 from rest_framework.status import HTTP_200_OK
 from rest_framework.test import APITestCase
 
-from .models import Refbook, RefbookVersion
+from .models import Refbook, RefbookVersion, RefbookElement
 
 
 class RefbooksAPITest(APITestCase):
@@ -62,6 +62,52 @@ class RefbooksAPITest(APITestCase):
                         "code": "ICD-10",
                         "name": " -10"
                     },
+                ]
+            } == content
+        )
+
+
+class RefbookElementsAPITest(APITestCase):
+    def setUp(self) -> None:
+        Refbook.objects.create(code="MS1", name=" ", description="")
+        Refbook.objects.create(code="ICD-10", name=" -10", description="")
+
+        RefbookVersion.objects.create(refbook_id='1', version='1.0', date="2023-08-09")
+        RefbookVersion.objects.create(refbook_id='2', version='1.0', date="2023-01-07")
+        RefbookVersion.objects.create(refbook_id='2', version='1.1', date="2023-08-09")
+        RefbookVersion.objects.create(refbook_id='2', version='1.2', date="2023-12-08")
+
+        RefbookElement.objects.create(version_id='1', code='c1', value='123')
+        RefbookElement.objects.create(version_id='2', code='b1', value='1234')
+        RefbookElement.objects.create(version_id='3', code='b2', value='12345')
+        RefbookElement.objects.create(version_id='3', code='b2-1', value='12345-1')
+        RefbookElement.objects.create(version_id='4', code='b3', value='123456')
+
+    def test_refbook_elements(self):
+        resp = self.client.get(reverse('refbook_elements', kwargs={"id": 2}))
+        self.assertEquals(resp.status_code, HTTP_200_OK)
+        content = resp.json()
+        print(content)
+        self.assertEquals(len(content.get("elements")), 2)
+        self.assertTrue(
+            {
+                'elements': [
+                    {'code': 'b2', 'value': '12345'},
+                    {'code': 'b2-1', 'value': '12345-1'},
+                ]
+            } == content
+        )
+
+    def test_refbook_elements_with_version(self):
+        resp = self.client.get(reverse('refbook_elements', kwargs={"id": 2}), data={'version': 1.0})
+        self.assertEquals(resp.status_code, HTTP_200_OK)
+        content = resp.json()
+        print(content)
+        self.assertEquals(len(content.get("elements")), 1)
+        self.assertTrue(
+            {
+                'elements': [
+                    {'code': 'b1', 'value': '1234'},
                 ]
             } == content
         )
--- project/refbooks/urls.py
+++ project/refbooks/urls.py
@@ -5,6 +5,6 @@ from . import api
 
 urlpatterns = [
     path('', api.RefbooksAPIView.as_view(), name='refbooks'),
-    path('<int:id>/elements', api.RefbookElementsAPIView.as_view(), name='refbook_elements'),
-    path('<int:id>/check_element', api.RefbookElementValidator.as_view(), name='check_element')
+    path('<str:id>/elements', api.RefbookElementsAPIView.as_view(), name='refbook_elements'),
+    path('<str:id>/check_element', api.RefbookElementValidator.as_view(), name='check_element')
 ]
